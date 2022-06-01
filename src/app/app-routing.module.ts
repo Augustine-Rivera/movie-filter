cfcdd2ccfcf035efc311e2d17eb959555cd34fc0
AustinRice71 <83186265+AustinRice71@users.noreply.github.com>
2022-06-01 19:39:21 -0400

    fixed links around webpage. Still need to fix anchor link to trailer. Needs styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MoviesComponent } from './moviesComponent/movies.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { MovieDetailComponent } from './movie-detail/movie-detail.component';
+import { InMemoryDataService } from './in-memory-data.service';
+
 
 const routes: Routes = [
   //path: string that matches url in browser address bar
@@ -12,14 +14,12 @@ const routes: Routes = [
   {path: 'dashboard', component: DashboardComponent},
   {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
   {path: 'detail/:id', component: MovieDetailComponent},
+  {path: 'movie/homepage', component: InMemoryDataService},
 ];
 
 //initialize router and startmto listen for browser location changes.
 @NgModule({
-  declarations: [],
-  imports: [
-    RouterModule.forRoot(routes)//home base
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]//make available throughout application.
 })
 export class AppRoutingModule { }
